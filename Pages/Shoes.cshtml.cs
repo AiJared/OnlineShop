1250c7fc054118e0dda7f3c5d7f51da327e7682b
Kerriea-star <ojared511@gmail.com>
2022-03-16 22:38:35 +0300

    Started developing a dynamic website using ASP.NET

---
+++ Pages/Shoes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineShop.Pages
+{
+    public class ShoesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
