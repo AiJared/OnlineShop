c5472607244289785051f10c3220671f6d0b3d9a
Kerriea-star <ojared511@gmail.com>
2022-03-17 12:38:08 +0300

    Debuging

--- Models/shoes.cs
+++ Models/shoes.cs
@@ -2,16 +2,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace OnlineShop.Models;
 
-public class Shoes
+public class Shoe
 {
     public int Id { get; set; }
 
     [Required]
     public string? Name { get; set; }
-    public ShoesSize Size { get; set; }
+    public ShoeSize Size { get; set; }
 
     [Range(0.01, 9999.99)]
     public decimal Price { get; set; }
 }
 
-public enum ShoesSize { Small, Medium, Large }
+public enum ShoeSize { Small, Medium, Large }
--- Services/ShoesService.cs
+++ Services/ShoesService.cs
@@ -1,24 +1,24 @@
 using OnlineShop.Models;
 
 namespace OnlineShop.Services;
-public static class ShoesService
+public static class ShoeService
 {
-    static List<Shoes> Shoes { get; }
+    static List<Shoe> Shoes { get; }
     static int nextId = 3;
     static ShoesService()
     {
-        Shoes = new List<Shoes>
+        Shoes = new List<Shoe>
                 {
-                    new Shoes { Id = 1, Name = "Knee High Boots", Price=20.00M, Size=ShoesSize.Large},
-                    new Shoes { Id = 2, Name = "Wellington Boots", Price=15.00M, Size=ShoesSize.Small}
+                    new Shoe { Id = 1, Name = "Knee High Boots", Price=20.00M, Size=ShoeSize.Large},
+                    new Shoe { Id = 2, Name = "Wellington Boots", Price=15.00M, Size=ShoeSize.Small}
                 };
     }
 
-    public static List<Shoes> GetAll() => Shoes;
+    public static List<Shoe> GetAll() => Shoes;
 
-    public static Shoes? Get(int id) => Shoes.FirstOrDefault(p => p.Id == id);
+    public static Shoe? Get(int id) => Shoes.FirstOrDefault(p => p.Id == id);
 
-    public static void Add(Shoes shoe)
+    public static void Add(Shoe shoe)
     {
         shoe.Id = nextId++;
         Shoes.Add(shoe);
@@ -33,7 +33,7 @@ public static class ShoesService
         Shoes.Remove(shoe);
     }
 
-    public static void Update(Shoes shoe)
+    public static void Update(Shoe shoe)
     {
         var index = Shoes.FindIndex(p => p.Id == shoe.Id);
         if (index == -1)
